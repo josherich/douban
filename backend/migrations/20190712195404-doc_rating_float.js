3a94652c71200f8223783a20676175c944654708
josherich <josherichchen@gmail.com>
2019-07-14 13:46:50 -0400

    add: migration

---
+++ backend/migrations/20190712195404-doc_rating_float.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.changeColumn('docs', 'rating', {
+       type: Sequelize.FLOAT,
+       defaultValue: 0.0
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.changeColumn('docs', 'rating', {
+       type: Sequelize.INTEGER,
+       defaultValue: 0
+    });
+  }
+};
