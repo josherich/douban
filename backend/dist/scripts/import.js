ebfe3cef9f130cbb4072dc9571b32d03a1c61610
josherich <josherichchen@gmail.com>
2019-07-27 15:42:10 -0400

    relaese: import with check uri unique

--- backend/dist/scripts/import.js
+++ backend/dist/scripts/import.js
@@ -8,6 +8,10 @@ var _models = _interopRequireDefault(require("../app/models"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 var Sequelize = require('sequelize');
 
 var fs = require('fs');
@@ -79,14 +83,16 @@ var createFromFile = function createFromFile(obj) {
       pubdate: meta['pubdate'],
       uri: obj['uri'],
       uuid: obj['uuid'],
-      summary: meta['abstracts']
+      summary: meta['abstracts'],
+      publisher: obj['page']
     };
   } catch (e) {
     return {
       title: obj['filename'].split('.')[0],
       uri: obj['uri'],
       uuid: obj['uuid'],
-      summary: obj['page']
+      summary: obj['page'],
+      publisher: obj['page']
     };
   }
 };
@@ -94,6 +100,38 @@ var createFromFile = function createFromFile(obj) {
 var docs_to_write = Object.keys(file_dict).map(function (id) {
   return createFromFile(file_dict[id]);
 });
+docs_to_write = docs_to_write.filter(
+/*#__PURE__*/
+function () {
+  var _ref = _asyncToGenerator(
+  /*#__PURE__*/
+  regeneratorRuntime.mark(function _callee(d) {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return _models["default"].Doc.findOne({
+              where: {
+                uri: d['uri']
+              }
+            });
+
+          case 2:
+            return _context.abrupt("return", _context.sent);
+
+          case 3:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function (_x) {
+    return _ref.apply(this, arguments);
+  };
+}());
 
 _models["default"].Doc.bulkCreate(docs_to_write).then(function () {
   console.log(docs_to_write.length + ' done.');
